7ad7566f16f12f5e22f776d09fe66a4fb98cc7cd
Antony Rinaldi <150718066+ElTonio313@users.noreply.github.com>
2025-05-27 16:29:27 -0300

    Update sistema_bancario_1.py

--- sistema_bancario_1.py
+++ sistema_bancario_1.py
@@ -10,19 +10,29 @@ menu = """
 saldo = 0
 limite = 500
 extrato = ""
-#quantidade_saques = 0
-#limite_saques = 3
+quantidade_saques = 0
+limite_saques = 3
 
 while True:
     opcao = input(menu)
 
     if opcao == "1":
         saque = float(input("Digite o valor a ser sacado \n>>"))
-        if saque < limite and saque <= saldo:
+        
+        excedeu_saldo = saque > saldo 
+        excedeu_limite = saque > limite
+        excedeu_saques = quantidade_saques > limite_saques
+
+        if excedeu_saldo:
+            print("Inválido, saldo excedido !!!")
+        elif excedeu_limite:
+            print("Inválido, limite excedido !!!")
+        elif excedeu_saques:
+            print("Inválido, limite de saques excedidos, máximo 3 !!!")
+        elif saque > 0:
             saldo -= saque
             extrato += f"Saque igual: {saque:.2f}\n"
-        else:
-            print("Valor inválido, tente novamente")
+            quantidade_saques += 1
 
     elif opcao == "2":
         valor = float(input("Digite o valor depositado \n>>"))
